375c856e5431602ad5c550d92fc78ed12262f9a6
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-06 22:26:37 +0000

    style: format js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,57 @@
-(function() {
-    //target doc sections
-    let form = document.querySelector('.contact-form');
-    let email = document.querySelector('#email');
+(function () {
+  //target doc sections
+  let form = document.querySelector(".contact-form");
+  let email = document.querySelector("#email");
 
-    //validate email function
-    function validateEmail() {
-        let value = email.value;
-        let hasAtSign = value.indexOf('@') > -1;
-        let hasPeriod = value.indexOf('.') > -1;
+  //validate email function
+  function validateEmail() {
+    let value = email.value;
+    let hasAtSign = value.indexOf("@") > -1;
+    let hasPeriod = value.indexOf(".") > -1;
 
-        if (!value) {
-            showErrorMessage(email, 'Email is required');
-            return false;
-        }
-
-        if (!hasAtSign || !hasPeriod) {
-            showErrorMessage(email, 'Not a valid email');
-            return false;
-        }
-
-        showErrorMessage(emailInput, null);
-        return true;
+    if (!value) {
+      showErrorMessage(email, "Email is required");
+      return false;
     }
 
-    //error message function
-    function showErrorMessage(input, message) {
-        let container = document.querySelector('.div__contact-form');
-
-        //remove existing message
-        let error = container.querySelector('.error-message');
-        if (error) {
-            container.removeChild(error);
-        }
-
-        //add error message
-        if (message) {
-            let error = document.createElement('div');
-            error.classList.add('error-message');
-            error.innerText = message;
-            container.appendChild(error);
-        }
+    if (!hasAtSign || !hasPeriod) {
+      showErrorMessage(email, "Not a valid email");
+      return false;
     }
 
-    //validate form function
-    function validateForm() {
-        return validateEmail;
+    showErrorMessage(emailInput, null);
+    return true;
+  }
+
+  //error message function
+  function showErrorMessage(input, message) {
+    let container = input.parentElement;
+
+    //remove existing message
+    let error = container.querySelector(".error-message");
+    if (error) {
+      container.removeChild(error);
     }
 
-    //submit success alert
-    form.addEventListener('submit', function (e) {
-        e.preventDefault(); //FORM IS NOT SUBMITTING TO SERVER
-        if (validateForm()) {
-            alert('Success');
-        }
-    });
+    //add error message
+    if (message) {
+      let error = document.createElement("div");
+      error.classList.add("error-message");
+      error.innerText = message;
+      container.appendChild(error);
+    }
+  }
 
+  //validate form function
+  function validateForm() {
+    return validateEmail();
+  }
+
+  //submit success alert
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); //FORM IS NOT SUBMITTING TO SERVER
+    if (validateForm()) {
+      alert("Success");
+    }
+  });
 })();
