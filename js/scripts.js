6b4528f276b01a1809d6cf70ea7d7eb85b92ccc9
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-06 22:05:50 +0000

    feat: add validate email function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 (function() {
-    function validateEmail() {
-        let form = document.querySelector('.contact-form');
-        let email = document.querySelector('#email');
+    //target doc sections
+    let form = document.querySelector('.contact-form');
+    let email = document.querySelector('#email');
 
+    //validate email function
+    function validateEmail() {
+        let value = email.value;
+        let hasAtSign = value.indexOf('@') > -1;
+        let hasPeriod = value.indexOf('.') > -1;
+        return value && hasAtSign && hasPeriod;
     }
 
 })();
