1d20b3e35292a568cde56458b97d16ed529222e0
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-06 22:02:22 +0000

    feat: target form and email in validate function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 (function() {
     function validateEmail() {
-        
+        let form = document.querySelector('.contact-form');
+        let email = document.querySelector('#email');
+
     }
 
 })();
