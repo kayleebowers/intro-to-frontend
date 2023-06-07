cdb7c4863412235743a7a1eed1d3ca4faeeb822d
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-07 12:47:28 +0000

    refactor: move error function up

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,25 @@
   let form = document.querySelector(".contact-form");
   let email = document.querySelector("#email");
 
+  //error message function
+  function showErrorMessage(input, message) {
+    let container = input.parentElement;
+
+    //remove existing message
+    let error = container.querySelector(".error-message");
+    if (error) {
+      container.removeChild(error);
+    }
+
+    //add error message
+    if (message) {
+      let error = document.createElement("div");
+      error.classList.add("error-message");
+      error.innerText = message;
+      container.appendChild(error);
+    }
+  }
+
   //validate email function
   function validateEmail() {
     let value = email.value;
@@ -23,33 +42,13 @@
     return true;
   }
 
-  //error message function
-  function showErrorMessage(input, message) {
-    let container = input.parentElement;
-
-    //remove existing message
-    let error = container.querySelector(".error-message");
-    if (error) {
-      container.removeChild(error);
-    }
-
-    //add error message
-    if (message) {
-      let error = document.createElement("div");
-      error.classList.add("error-message");
-      error.innerText = message;
-      container.appendChild(error);
-    }
-  }
-
   //validate form function
   function validateForm() {
     return validateEmail();
   }
 
   //submit success alert
-  let submitButton = document.querySelector('#contact-form__submit');
-  submitButton.addEventListener("click", function (e) {
+  form.addEventListener("submit", function (e) {
     e.preventDefault(); //FORM IS NOT SUBMITTING TO SERVER
     if (validateForm()) {
       alert("Success");
